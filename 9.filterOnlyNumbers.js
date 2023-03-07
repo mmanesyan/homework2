44b94f384fdffc303ff973802196f8411eff3c49
Mane Manesyan <manemanesyan062@gmail.com>
2023-03-07 21:08:43 +0400

    Changed the task's solution

--- 9.filterOnlyNumbers.js
+++ 9.filterOnlyNumbers.js
@@ -1,10 +1,4 @@
 function onlyNumbers(arr){
-    let newArray=[]
-    for(let i=0;i<arr.length;i++){
-        if(typeof arr[i]==="number" && !isNaN(arr[i])){
-            newArray.push(arr[i])
-        }
-    }
-    return newArray
+    return arr.filter(element => typeof element==="number" && !isNaN(element))
 }
 console.log(onlyNumbers([1, 2, 'a', true, {}, undefined, 55, false, 'hi', null, 166, NaN]))
