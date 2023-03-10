3029a4fe9619fc1fc9e9aa49822e07a4f6aa7d45
Mane Manesyan <manemanesyan062@gmail.com>
2023-03-10 18:32:23 +0400

    Added the implementation of slice method.

---
+++ slice.js
@@ -0,0 +1,16 @@
+function slice(array, start = 0, end = array.length) {
+    if (start < 0) {
+      start = array.length + start;
+    }
+    if (end < 0) {
+      end = array.length + end;
+    }
+    let result = [];
+    for (let i = start; i < end; i++) {
+      result.push(array[i]);
+    }
+    return result
+  }
+  
+  console.log(slice([1,2,3,9,7], -1))
+  
