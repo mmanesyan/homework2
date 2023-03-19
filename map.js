f73a9b8cd0a47399a4e6cbe40acd09fe3c7f3fae
Mane Manesyan <manemanesyan062@gmail.com>
2023-03-19 18:05:17 +0400

    Changed the implementation of map method.

--- map.js
+++ map.js
@@ -1,8 +1,16 @@
-function map(array){
-    let result=[]
-    for(let i=0;i<array.length;i++){
-        const currentItem = cb(array[i], i, array);
-        result.push(currentItem)
+function map(array, callback) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+      const changedItem = callback(array[i], i, array)
+      result.push(changedItem)
     }
     return result
-}
+  }
+  function check(number) {
+    return number+2
+  }
+  const numbers = [1, 2, 3, 4]
+  const x = map(numbers, check)
+  console.log(x)
+  
+  
